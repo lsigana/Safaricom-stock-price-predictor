954c8f8daa980174999c94d47581be388ad15a0e
Leah Sigana <160542207+arieltesfaye@users.noreply.github.com>
2025-01-30 15:36:13 +0300

    wgsi.py correction

--- stock_predictor/wsgi.py
+++ stock_predictor/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
 import sys
+import os  # Missing import
 from pathlib import Path
+from django.core.wsgi import get_wsgi_application
 
+project_path = '/home/lsigana/Safaricom-stock-price-predictor'
+
+if project_path not in sys.path:
+    sys.path.append(project_path)
+
+# Set Django settings module BEFORE calling get_wsgi_application()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock_predictor.settings')
 
 application = get_wsgi_application()
+
